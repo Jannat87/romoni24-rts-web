be8a782ef0dc0dd20eddb5e5b9f171a02d4043d1
Jannat87 <fatemajannat.cse@gmail.com>
2024-10-20 23:52:49 +0600

    another file uploded 202410201152

--- src/components/global/Menu.tsx
+++ src/components/global/Menu.tsx
@@ -4,7 +4,7 @@ const Menu = () => {
     return (
         <>
             <div>
-                <a href="/"> Home </a>
+                {/* <a href="/"> Home </a>
                 <a href="/rupcorca"> Rupcorca </a>
                 <a href="/sisur-jotno"> Sisur Jotno </a>
                 <a href="/ranna-o-ranna-ghor"> Ranna O rannaghor </a>
@@ -12,7 +12,7 @@ const Menu = () => {
                 <a href="/ghorer-jotno"> Ghorer Jotno </a>
                 <a href="/uddogta-corner"> Uddogta Corner </a>
                 <a href="/contact"> Contact </a>
-                <a href="/about"> About </a>
+                <a href="/about"> About </a> */}
             </div> 
         </>
     );
